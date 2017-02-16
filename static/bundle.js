e35bebe5a9cc56966172781a1ddbb948b65d7682
Paul Oliva <paulmoliva@gmail.come>
2017-02-15 23:19:57 -0800

    fix flicker

--- static/bundle.js
+++ static/bundle.js
@@ -66570,7 +66570,7 @@
 	              _this2.props.addResults(message);
 	            }
 	
-	            if (message.name && _this2.state.pollName === '') {
+	            if (message.name) {
 	              _this2.setState({ pollName: message.name });
 	            }
 	            // if (data.message.vote) {
